6565ec760acb16d22685d6276f6c8d3d21a4485b
junhuizhou <jzhou3087@berkeley.edu>
2017-11-27 15:08:13 -0800

    jun's first commit

--- simanneal.py
+++ simanneal.py
@@ -1,17 +1,76 @@
+
+
+# in20_0 = open("inputs20/input20_0.in", "r")
+# wizNum = int(in20_0.readline())
+# conNum = int(in20_0.readline())
+# print wizNum
+# print conNum
+# nameSet = set()
+# names = in20_0.readlines()
+# for line in names:
+# 	lists = line.split(' ')
+# 	nameSet.add(lists[0])
+# 	nameSet.add(lists[1])
+# 	nameSet.add(lists[2])
+
+# firstChoose = list(nameSet)
+# shuffle(firstChoose)
 from random import shuffle
+import argparse
 
-in20_0 = open("inputs20/input20_0.in", "r")
-wizNum = int(in20_0.readline())
-conNum = int(in20_0.readline())
-print wizNum
-print conNum
-nameSet = set()
-names = in20_0.readlines()
-for line in names:
-	lists = line.split(' ')
-	nameSet.add(lists[0])
-	nameSet.add(lists[1])
-	nameSet.add(lists[2])
+"""
+======================================================================
+  Complete the following function.
+======================================================================
+"""
 
-firstChoose = list(nameSet)
-shuffle(firstChoose)
+def solve(num_wizards, num_constraints, wizards, constraints):
+    """
+    Write your algorithm here.
+    Input:
+        num_wizards: Number of wizards
+        num_constraints: Number of constraints
+        wizards: An array of wizard names, in no particular order
+        constraints: A 2D-array of constraints, 
+                     where constraints[0] may take the form ['A', 'B', 'C']i
+
+    Output:
+        An array of wizard names in the ordering your algorithm returns
+    """
+    return []
+
+"""
+======================================================================
+   No need to change any code below this line
+======================================================================
+"""
+
+def read_input(filename):
+    with open(filename) as f:
+        num_wizards = int(f.readline())
+        num_constraints = int(f.readline())
+        constraints = []
+        wizards = set()
+        for _ in range(num_constraints):
+            c = f.readline().split()
+            constraints.append(c)
+            for w in c:
+                wizards.add(w)
+                
+    wizards = list(wizards)
+    return num_wizards, num_constraints, wizards, constraints
+
+def write_output(filename, solution):
+    with open(filename, "w") as f:
+        for wizard in solution:
+            f.write("{0} ".format(wizard))
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description = "Constraint Solver.")
+    parser.add_argument("input_file", type=str, help = "___.in")
+    parser.add_argument("output_file", type=str, help = "___.out")
+    args = parser.parse_args()
+
+    num_wizards, num_constraints, wizards, constraints = read_input(args.input_file)
+    solution = solve(num_wizards, num_constraints, wizards, constraints)
+    write_output(args.output_file, solution)
