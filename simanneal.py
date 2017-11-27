33f01c1bc4a24875800e224d0136dca4c6dac828
David Luo <davidluo@berkeley.edu>
2017-11-27 14:29:10 -0800

    First Commit

---
+++ simanneal.py
@@ -0,0 +1,17 @@
+from random import shuffle
+
+in20_0 = open("inputs20/input20_0.in", "r")
+wizNum = int(in20_0.readline())
+conNum = int(in20_0.readline())
+print wizNum
+print conNum
+nameSet = set()
+names = in20_0.readlines()
+for line in names:
+	lists = line.split(' ')
+	nameSet.add(lists[0])
+	nameSet.add(lists[1])
+	nameSet.add(lists[2])
+
+firstChoose = list(nameSet)
+shuffle(firstChoose)
