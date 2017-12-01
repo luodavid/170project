bc644f58f844d1a4754f88431562ba28982a042c
Raymond Ly <lyraymond53@berkeley.edu>
2017-11-30 23:32:39 -0800

    staff sols through 240

--- runsim.py
+++ runsim.py
@@ -11,24 +11,24 @@ outputFile = [f for f in listdir("./outputs")]
 
 for i in filesStaff:
 	out = "output" + i[5:-3] + ".out"
-# 	subprocess.call(["python2", "newsim.py", "./inputs/inputsStaff/" + i, out])
+	subprocess.call(["python2", "newsim.py", "./inputs/inputsStaff/" + i, out])
 	subprocess.call(["python2", "output_validator.py", "./inputs/inputsStaff/" + i, out])
 	print out
 
-for i in files20:
-	out = "output" + i[5:-3] + ".out"
+# for i in files20:
+	# out = "output" + i[5:-3] + ".out"
 	# subprocess.call(["python2", "newsim.py", "./inputs/inputs20/" + i, out])
-	subprocess.call(["python2", "output_validator.py", "./inputs/inputs20/" + i, out])
-	print out
+	# subprocess.call(["python2", "output_validator.py", "./inputs/inputs20/" + i, out])
+	# print out
 
-for i in files35:
-	out = "output" + i[5:-3] + ".out"
+# for i in files35:
+	# out = "output" + i[5:-3] + ".out"
 	# subprocess.call(["python2", "newsim.py", "./inputs/inputs35/" + i, out])
-	subprocess.call(["python2", "output_validator.py", "./inputs/inputs35/" + i, out])
-	print out
+	# subprocess.call(["python2", "output_validator.py", "./inputs/inputs35/" + i, out])
+	# print out
 
-for i in files50:
-	out = "output" + i[5:-3] + ".out"
+# for i in files50:
+	# out = "output" + i[5:-3] + ".out"
 	# subprocess.call(["python2", "newsim.py", "./inputs/inputs50/" + i, out])
-	subprocess.call(["python2", "output_validator.py", "./inputs/inputs50/" + i, out])
-	print out
+	# subprocess.call(["python2", "output_validator.py", "./inputs/inputs50/" + i, out])
+	# print out
